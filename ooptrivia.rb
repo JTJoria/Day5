780ce7a81b4fd594ee8feec74a1679188c8f5c2a
TIM JORIA <jtjoria@gmail.com>
2016-01-07 21:17:00 -0600

    end_of_day

--- ooptrivia.rb
+++ ooptrivia.rb
@@ -4,14 +4,13 @@
 # 4. CHALLENGE: Change the program to keep track of number right/wrong and give a score at the end.
 # 5. CHALLENGE: Change the program to give the user the choice at the end of the game to retry the cards they got wrong.
 # 6. CHALLENGE: Change the interface with better prompts, ASCII art, etc. Be as creative as you'd like!
-​
-class Card
+​class Card
 
-  attr_reader :question, :user_answer
+  # attr_reader :question, :answer
 
-  def initialize  
+  def initialize (question, answer)
     @question = question
-    @user_answer = user_answer
+    @answer = answer
   end
 
   def question
@@ -23,46 +22,43 @@ class Card
   end
   
 end
-​
-class Deck
 
-  attr_reader :draw_card, :remaining_cards
+class Deck 
 
-  def initialize (draw_card, remaining_cards)
-    card = Card.new
-    @card = []
-    @draw_card = draw_card
-    @remaining_cards = remaining_cards
+  # attr_reader :draw_card, :remaining_cards
+
+  def initialize(trivia_data)
+    @card_instances = []
+    trivia_data.each do |k, v|
+      @card_instances << Card.new(k, v)
+    end
   end
-
 
   def draw_card
-    
+
   end
-  
 
   def remaining_cards
 
   end
 
 end
-​
-​
+
 trivia_data = {
   "What is the capital of Illinois?" => "Springfield",
   "Is Africa a country or a continent?" => "Continent",
   "Tug of war was once an Olympic event. True or false?" => "True"
 }
 
-deck = Deck.new(trivia_data) # deck is an instance of the Deck class
-​
-while deck.remaining_cards > 0
-  card = deck.draw_card # card is an instance of the Card class
-  puts card.question
-  user_answer = gets.chomp
-  if user_answer.downcase == card.answer.downcase
-    puts "Correct!"
-  else
-    puts "Incorrect!"
-  end
-end
+p deck = Deck.new(trivia_data) # deck is an instance of the Deck class
+
+# while deck.remaining_cards > 0
+#   card = deck.draw_card # card is an instance of the Card class
+#   puts card.question
+#   user_answer = gets.chomp
+#   if user_answer.downcase == card.answer.downcase
+#     puts "Correct!"
+#   else
+#     puts "Incorrect!"
+#   end
+# end
